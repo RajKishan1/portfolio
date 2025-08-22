5cec9483e32946afaf4562d4b8d44601ddca64ff
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-08-22 16:42:29 +0530

    fix:sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import Menu from "./Menu";
 
 const Sidebar = () => {
   return (
-    <div className="w-[15%] border p-4 gap-2.5">
+    <div className="min-h-screen w-[15%] border p-4 gap-2.5 bg-neutral-50 text-black flex flex-col justify-between">
       <div className="flex ">
         <div>
           <img src="/me.png" alt="my pic" className="rounded-full size-12" />
@@ -18,9 +18,12 @@ const Sidebar = () => {
       </div>
       <div>
         <h5>Socials</h5>
-        
       </div>
-      <div></div>
+      <div>
+        <button className="bg-neutral-900 rounded-full font-semibold text-white px-4 py-3 text-sm">
+          Read Resume
+        </button>
+      </div>
     </div>
   );
 };
