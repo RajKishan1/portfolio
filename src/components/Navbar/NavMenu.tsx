7ea5ccee90f46b5799e581d8df01c9467ef06132
Raj Kishan Verma <rajkishanvrma@gmail.com>
2025-08-25 14:32:58 +0530

    fix:homepage

--- src/components/Navbar/NavMenu.tsx
+++ src/components/Navbar/NavMenu.tsx
@@ -7,31 +7,31 @@ import { FaUserCircle } from "react-icons/fa";
 import { IoSunny } from "react-icons/io5";
 
 const Class =
-  "flex gap-1.5 items-center rounded-full hover:border-neutral-600 hover:bg-neutral-800 px-3 py-0 ";
+  "flex gap-1.5 items-center rounded-full hover:border-neutral-600 hover:bg-neutral-800 px-3 py-2 ";
 const NavMenu = () => {
   return (
     <div className="bg-black rounded-full px-2 py-1.5 flex gap-3 items-center border-[1px] border-neutral-800 text-sm">
-      <div className="rounded-full hover:bg-neutral-700 p-2.5">
+      <a href="/" className="rounded-full hover:bg-neutral-700 p-2.5">
         <AiFillHome />
-      </div>
+      </a>
       <div className="flex px-4 border-r border-l border-neutral-500">
         {" "}
-        <div className={Class}>
+        <a href="/about" className={Class}>
           <FaUserCircle />
           About
-        </div>
-        <div className={Class}>
+        </a>
+        <a href="/work" className={Class}>
           <FaNetworkWired />
           Work
-        </div>
-        <div className={Class}>
+        </a>
+        <a href="/blog" className={Class}>
           <LuNotebookText />
           Blog
-        </div>
-        <div className={Class}>
+        </a>
+        <a href="/gallery" className={Class}>
           <GrGallery />
           Gallery
-        </div>
+        </a>
       </div>
       <div className="rounded-full hover:bg-neutral-700 p-2.5">
         <IoSunny />
